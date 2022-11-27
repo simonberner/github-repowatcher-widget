39256c6fdad3a9450fbe8262529a4430a6be1570
Simon Berner <sibebzh@gmail.com>
2022-11-27 16:22:21 +0100

    add: :sparkles: add default widget

---
+++ RepoWatcherWidget/RepoWatcherWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherWidgetBundle.swift
+//  RepoWatcherWidget
+//
+//  Created by Simon Berner on 27.11.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RepoWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RepoWatcherWidget()
+        RepoWatcherWidgetLiveActivity()
+    }
+}
