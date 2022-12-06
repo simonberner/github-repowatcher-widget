ee80399fc5ddfbea3269607ff8f74ec40a803262
Simon Berner <sibebzh@gmail.com>
2022-12-06 19:26:19 +0100

    add: :sparkles: add contributors mock data

--- RepoWatcherWidget/MockData.swift
+++ RepoWatcherWidget/MockData.swift
@@ -10,21 +10,25 @@ import Foundation
 struct MockData {
     // mock data object
     static let repoOne = Repository(name: "Repository 1",
-                                       owner: Owner(avatarUrl: ""),
-                                       hasIssues: true,
-                                       forks: 66,
-                                       watchers: 250,
-                                       openIssues: 88,
-                                       pushedAt: "2022-10-27T19:00:59Z",
-                                        avatarData: Data())
+                                    owner: Owner(avatarUrl: ""),
+                                    hasIssues: true,
+                                    forks: 66,
+                                    watchers: 250,
+                                    openIssues: 88,
+                                    pushedAt: "2022-10-27T19:00:59Z",
+                                    avatarData: Data(),
+                                    contributors: [Contributor(login: "Simon Berner", avatarUrl: "", contributions: 23, avatarData: Data()),
+                                                   Contributor(login: "Sean Penn", avatarUrl: "", contributions: 33, avatarData: Data()),
+                                                   Contributor(login: "Alicia Keys", avatarUrl: "", contributions: 99, avatarData: Data()),
+                                                   Contributor(login: "Minion Mini", avatarUrl: "", contributions: 45, avatarData: Data())])
 
     // mock data object
     static let repoTwo = Repository(name: "Repository 2",
-                                       owner: Owner(avatarUrl: ""),
-                                       hasIssues: false,
-                                       forks: 199,
-                                       watchers: 856,
-                                       openIssues: 26,
-                                       pushedAt: "2022-01-27T19:00:59Z",
-                                        avatarData: Data())
+                                    owner: Owner(avatarUrl: ""),
+                                    hasIssues: false,
+                                    forks: 199,
+                                    watchers: 856,
+                                    openIssues: 26,
+                                    pushedAt: "2022-01-27T19:00:59Z",
+                                    avatarData: Data())
 }
